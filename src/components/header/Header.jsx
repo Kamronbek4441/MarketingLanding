12da50fa0e30d9693d700e42e1925b3934082790
unknown <kamronbekdilshadov24@gmail.com>
2026-01-13 23:08:47 +0500

    Sayt 100% bitti

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,55 +4,25 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <header>
-        <div class="container">
-          <div class="logo-section">
-            <div class="menu-toggle">
-              <img src="./assets/toggle.jpg" alt="" />
-            </div>
-            <div class="logo">
-              <img src="/assets/logo.jpg" alt="УПАКУЕМ.РФ" />
-              <div class="company-text">
-                <span>Маркетинговое агентство</span>
-                <span>Маркетинг система продажи</span>
-              </div>
-            </div>
-          </div>
-          <nav class="nav-links">
-            <ul>
-              <li>
-                <a href="#">УСЛУГИ</a>
-              </li>
-              <li>
-                <a href="#">ОТРАСЛЕВЫЕ РЕШЕНИЯ</a>
-              </li>
-              <li>
-                <a href="#">ПРОЕКТЫ</a>
-              </li>
-              <li>
-                <a href="#">БЛОГ</a>
-              </li>
-              <li>
-                <a href="#">КОМПАНИЯ</a>
-              </li>
-              <li>
-                <a href="#">КОНТАКТЫ</a>
-              </li>
-            </ul>
-          </nav>
-          <div class="social-phone">
-            <div class="social-icons">
-              <img src="/assets/icons.jpg" alt="" />
-            </div>
-            <div class="phone">
-              <a href="tel:88007007388">8 (800) 700-73-88</a>
-              <a href="#" class="callback">
-                ЗАКАЗАТЬ ЗВОНОК
-              </a>
-            </div>
-          </div>
-        </div>
-      </header>
+      <div className="head1">
+        <img src="/assets/logo.jpg" alt="" />
+        <h2>
+          Маркетинговое агенство <br /> Маркетинг система продажи
+        </h2>
+        <img src="/assets/icons.jpg" alt="" />
+        <h1 className="margin">
+          8 (800) 700-73-88 <br /> <span className="grey">заказать звонок</span>
+        </h1>
+      </div>
+      <div className="case-divider1"></div>
+      <div className="head2">
+        <h2 className="link1">Услуги</h2>
+        <h2 className="link1">Отпаслевые Решения</h2>
+        <h2 className="link1">Проекты</h2>
+        <h2 className="link1">Блог</h2>
+        <h2 className="link1">Компания</h2>
+        <h2 className="link1">Контакты</h2>
+      </div>
     </div>
   );
 };
